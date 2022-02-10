563d3891aa9c9d5259e0ef0841d94d1079063cac
admin_g0vna <76201619+adming0vna@users.noreply.github.com>
2022-02-09 23:25:33 -0500

    Add files via upload

---
+++ models/PrivateMessage.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const PrivateMessageSchema = new mongoose.Schema({
+  from_user: {
+    type: String,
+    required: true,
+  },
+  to_user: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date_sent: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+const PrivateMessage = mongoose.model("PrivateMessage", PrivateMessageSchema);
+module.exports = PrivateMessage;
